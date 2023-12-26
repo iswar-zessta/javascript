d9079eeec5f7cff8f4ff92fb7caf139cd6ed7d0b
Iswar Chandra Rana <iswar.rana@gmail.com>
2023-12-26 12:34:23 +0530

    added date and time

--- 01_basics/07_datesinJs.js
+++ 01_basics/07_datesinJs.js
@@ -6,7 +6,7 @@ let myDate = new Date()
 // console.log(myDate.toLocaleString());
 // console.log(typeof myDate);
 
-// let myCreatedDate = new Date(2023, 0, 23)
+// let myCreatedDate = new Date(2024, 0, 16)
 // let myCreatedDate = new Date(2023, 0, 23, 5, 3)
 // let myCreatedDate = new Date("2023-01-14")
 let myCreatedDate = new Date("01-14-2023")
